d852fff7adc13188afa31e561f2a7f8c725b361d
Vivian Le <vivianhdle@gmail.com>
2019-03-22 13:07:40 -0700

    added get data endpoint in full

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 
 //to load files, npm install package --save  **DO THIS IN THE SERVER FILE, WHERE YOUR PACKAGE IS.
 const express = require('express'); //load the express library into the file
-const mysql = require('mysql');
+const mysql = require('mysql');//load mysql library
 const mysqlcredentials = require('./mysqlcreds.js');
+//using the credentials that we loaded, establish a preliminary connection to the database
 const db = mysql.createConnection(mysqlcredentials);//hey mysql library, make a connection to the database, use these credentials to make that connection
 
 const server = express();
@@ -12,39 +13,39 @@ server.use( express.static(__dirname +'/html') );//serving index.html, takes in 
 //__dirname is your current working directory
 //only works on static files
 
+server.use(express.urlencoded({extended:false}));//have express pull body data that is url encoded and place it into an object called body
+
 
 //takes in two parameters, 
 //the path to listen for,
 //the callback function to call once that path has been received
+//parameters: an object representing all of the data coming from the client to the server
+//and an object representing all of the data going from the server to the client
 server.get('/api/grades', (req,res)=>{//could be req and res
-    res.send(`{
-        "success": true,
-        "data": [{
-            "id": 1,
-            "name": "Vivian Le",
-            "course": "Linear Algebra",
-            "grade": 80
-        }, {
-            "id": 2,
-            "name": "Spongebob Squarepants",
-            "course": "Pineapple",
-            "grade": 100
-        }, {
-            "id": 3,
-            "name": "Squidward Null",
-            "course": "Squidwardian History",
-            "grade": 30
-        }, {
-            "id": 4,
-            "name": "Mr Krabs",
-            "course": "Money",
-            "grade": 60
-        }]
-    }`)
+    db.connect(()=>{
+        const query = 'SELECT `id`, CONCAT(`givenname`," ",`surname`) AS `name`, `course`,`grade` FROM grades';
+        db.query(query,(error,data,)=>{
+            const output ={
+                success:false
+            }
+            if (!error){
+                output.success = true;
+                output.data = data;
+            } else {
+                output.error = error;
+            }
+            res.send(output);
+        })
+    });
     
-    //parameters: an object representing all of the data coming from the client to the server
-    //and an object representing all of the data going from the server to the client
 })//called an endpoint
+
+
+server.post('/api/grades',(request,response)=>{
+    db.c
+})
+
+
 
 
 //listen is a method in server that requires 2 parameters, the port and a callback function
